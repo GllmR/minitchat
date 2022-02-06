de8d2333668300d87978016d328417e8c8127c26
gllmr <guillaume@livingdata.co>
2022-02-06 13:03:58 +0100

    Move scrollTop

--- public/script.js
+++ public/script.js
@@ -10,7 +10,7 @@ const chatWindow = document.querySelector('.chat-window')
 const usersList = document.querySelector('.users-list')
 const emojis = document.querySelectorAll('.chat-btn')
 const notifications = document.querySelector('.notifications')
-let messages
+let messages = []
 
 // Check if a name is in the localstorage
 let name = localStorage.getItem('name') || null
@@ -71,8 +71,9 @@ function miniChat(socket, name) {
     if (messages?.length !== msgs.length) {
       msgs.forEach(msg => {
         chatWindow.insertBefore(renderMessage(msg), chatWindow.childNodes[0])
-        chatWindow.scrollTop = chatWindow.scrollHeight
       })
+      
+      chatWindow.scrollTop = chatWindow.scrollHeight
     }
 
     messages = msgs
@@ -103,7 +104,7 @@ function miniChat(socket, name) {
   })
 
 // Hello to stalker
-  console.log('%cTu regardes quoi ' + name + ' ?', 'color: deeppink; background-color: black; border: 1px solid lime; font-size: 3vw; margin: 8px;')
+  console.log('%cTu regardes quoi ' + name + ' ?', 'color: deeppink; background-color: black; border: 1px solid lime; font-size: 3vw; padding: 2%; margin-bottom: 25px;')
 }
 
 // 🍪 Ask for username 𝕱 𝕺 𝕽 𝕰 𝖁 𝕰 𝕽 👾
