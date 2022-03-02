c5399f7926d13b231a99d22a234d02e586b50166
gllmr <guillaume@livingdata.co>
2022-03-02 21:31:56 +0100

    Fix unformated date in liens page

--- public/liens/script.js
+++ public/liens/script.js
@@ -1,15 +1,17 @@
 const socket = io()
+import {formatDate} from '../utils.js'
 
 const linksWindow = document.querySelector('.links-window')
 
 function renderLinks(link) {
   const div = document.createElement('div')
+  const formatedDate = formatDate(link.time)
   div.classList.add('render-message')
 
   div.innerHTML =
     `<div class="message">
       <span class="time">
-        ${link.time}
+        ${formatedDate}
       </span> <span>${link.text}</span>
     </div>`
 
